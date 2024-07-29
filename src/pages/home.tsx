4c83f4d030bebb751cdc1c286a172f9663a3ab58
song <songikim4464@gmail.com>
2024-07-29 23:23:46 +0900

    style: main page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,20 @@ const HomePage = () => {
   return (
     <div className="main">
       <Nav />
-      <div>Homepage</div>
+      <div className="content">
+        <div className="text">
+          <div className="title">
+            김송이 <br /> 프론트엔드 포트폴리오
+          </div>
+          <div className="sub">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Habitant
+            cras morbi hendrerit nunc vel sapien. In habitasse at diam
+            suspendisse non vitae fermentum, pharetra arcu. Viverra a morbi ut
+            donec in. Ac diam, at sed cras nisi.
+          </div>
+        </div>
+        <div className="img">image</div>
+      </div>
     </div>
   );
 };
